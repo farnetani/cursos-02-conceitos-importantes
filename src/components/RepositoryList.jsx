65a4f5411a07befd2bcb9faa0ecf991cda8c5915
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-03-22 18:15:39 -0300

    Propriedades no React

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,20 @@
+import { RepositoryItem } from './RepositoryItem'
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+}
+
+export function RepositoryList() {
+  return (
+    <section className="repository-List">
+      <h1>Lista de repositórios</h1>
+      <RepositoryItem repository={repository} />
+      <RepositoryItem repository={repository} />
+      <RepositoryItem repository={repository} />
+      <RepositoryItem repository={repository} />
+      <ul></ul>
+    </section>
+  )
+}
