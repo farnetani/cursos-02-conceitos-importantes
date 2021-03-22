a2825d4a85714ef5b9dfab0a71bd21431943a6e8
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-03-22 18:42:48 -0300

    A imutabilidade no React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 
 // Toda função que começa com use = Hook (= gancho)
 
+// Conceito da Imutabilidade : não devemos alterar um valor na memória e sim criar um novo valor conforme explicado abaixo
+// usuarios = ['junior', 'virginia', 'lucca']
+// usuarios.push('miguel')
+
+// # Conceito da Imutabilidade : pegamos e criamos um novo Array e aí sim adicionamos o que queremos
+// novoUsuarios = [...usuarios, 'miguel']
+
 export function Counter() {
   // useState(0) retorna um array com 2 posições
   const [counter, setCounter] = useState(0)
